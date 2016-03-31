0c1d4bfce85c95fa1a6fe4bd062d8bfb6b80abef
Steve Fraser <swbfraser@gmail.com>
2016-03-30 22:55:04 -0500

    Extracted Methods from the constructor.

--- MineSweeper/src/minesweeper/domain/MineField.java
+++ MineSweeper/src/minesweeper/domain/MineField.java
@@ -20,12 +20,108 @@ class MineField {
 				
 		squares = new HashMap(numberOfSquares);
 
-		//Default all square to blank
-		for(int i=0;i<numberOfSquares;i++) {
-			squares.put(new Integer(i),new BlankSquare(i));
-		}
+		defaultAllSquaresToBlank();
+	
+		generateRandomMineLocations(mines);
 
-		//Generate Random mine locations	
+		for(Iterator it = mineLocations.iterator();it.hasNext();) {
+			Integer location = (Integer)it.next();
+			Square mine = new MineSquare(location.intValue());
+			squares.put(location,mine);
+
+			List neighbors = getNeighbors(width, location);
+
+			incrementNeighborNumberSquares(neighbors);
+		}		
+
+		squaresKnowTheirNeighborsForReveiling(width);
+	}
+
+	private void squaresKnowTheirNeighborsForReveiling(int width) {
+		for(int location =0;location<numberOfSquares;location++) {
+
+			Square square = (Square)squares.get(new Integer(location));
+			
+			//Find Neighbor Squares.
+			List neighbors = getNeighbors(width, location);
+
+			for(Iterator it = neighbors.iterator();it.hasNext();) {
+				square.addNeighbor((Square)(squares.get((Integer)it.next())));
+			}
+		}
+	}
+
+	private void incrementNeighborNumberSquares(List neighbors) {
+		Integer location;
+		for(Iterator it2 = neighbors.iterator();it2.hasNext();) {
+			location = (Integer)it2.next();
+
+			//Increment Neighboring Number Squares
+			if (squares.get(location) instanceof NumberSquare) {
+				NumberSquare ns = (NumberSquare)squares.get(location);
+				ns.incrementValue();
+			//Make Neighbor Number Square	
+			}else if(squares.get(location) instanceof BlankSquare) {
+				squares.put(location,new NumberSquare(location.intValue(),1));
+			}
+		}
+	}
+
+	private List getNeighbors(int width, Integer location) {
+		List neighbors = new ArrayList();
+
+		// if location is not on left side, then add neighbors from left
+		addLeftNeighbors(width, location, neighbors);
+
+		// if location is not on right side, then add neighbors from right
+		addRightNeighbors(width, location, neighbors);
+
+		// if location is not on top, then add neighbors from top
+		addTopNeighbors(width, location, neighbors);
+
+		// if location is not on bottom, then add neighbors from bottom
+		addBottomNeighbors(width, location, neighbors);
+		return neighbors;
+	}
+
+	private void addBottomNeighbors(int width, Integer location, List neighbors) {
+		if (location< numberOfSquares - width) { 
+			neighbors.add(new Integer(location+width));
+		}
+	}
+
+	private void addTopNeighbors(int width, Integer location, List neighbors) {
+		if (location>width - 1) {  
+			neighbors.add(new Integer(location-width));
+		}
+	}
+
+	private void addRightNeighbors(int width, Integer location, List neighbors) {
+		if ((location+1)%(width)!=0){ 
+			neighbors.add(new Integer(location+1));
+			if (location>width-1) {
+				neighbors.add(new Integer(location-(width-1)));
+			}
+
+			if (location<numberOfSquares-width) {
+				neighbors.add(new Integer(location+(width + 1)));
+			}
+		}
+	}
+
+	private void addLeftNeighbors(int width, Integer location, List neighbors) {
+		if (location%width!=0) {
+			neighbors.add(new Integer(location-1));
+			if (location>width-1) {
+				neighbors.add(new Integer(location-(width + 1)));
+			}
+			if (location<numberOfSquares - width) {
+				neighbors.add(new Integer(location+(width-1)));
+			}
+		}
+	}
+
+	private void generateRandomMineLocations(int mines) {
 		mineLocations = new ArrayList(mines);
 		for(int i=0;i<mines;i++) {
 			Integer random = null;
@@ -36,110 +132,11 @@ class MineField {
 
 			mineLocations.add(random);
 		}
-		
-		
-		// Add Mine Squares
-		for(Iterator it = mineLocations.iterator();it.hasNext();) {
-			Integer location = (Integer)it.next();
-			Square mine = new MineSquare(location.intValue());
-			squares.put(location,mine);
+	}
 
-			//Get Neighbor Squares
-			List neighbors = new ArrayList();
-
-			// if location is not on left side, then add neighbors from left
-			if (location%width!=0) {
-				neighbors.add(new Integer(location-1));
-				if (location>width-1) {
-					neighbors.add(new Integer(location-(width + 1)));
-				}
-				if (location<numberOfSquares - width) {
-					neighbors.add(new Integer(location+(width-1)));
-				}
-			}
-
-			// if location is not on right side, then add neighbors from right
-			if ((location+1)%(width)!=0){ 
-				neighbors.add(new Integer(location+1));
-				if (location>width-1) {
-					neighbors.add(new Integer(location-(width-1)));
-				}
-
-				if (location<numberOfSquares-width) {
-					neighbors.add(new Integer(location+(width + 1)));
-				}
-			}
-
-			// if location is not on top, then add neighbors from top
-			if (location>width - 1) {  
-				neighbors.add(new Integer(location-width));
-			}
-
-			// if location is not on bottom, then add neighbors from bottom
-			if (location< numberOfSquares - width) { 
-				neighbors.add(new Integer(location+width));
-			}
-
-				
-			//Increment or Make neighboring number squares
-			for(Iterator it2 = neighbors.iterator();it2.hasNext();) {
-				location = (Integer)it2.next();
-
-				//Increment Neighboring Number Squares
-				if (squares.get(location) instanceof NumberSquare) {
-					NumberSquare ns = (NumberSquare)squares.get(location);
-					ns.incrementValue();
-				//Make Neighbor Number Square	
-				}else if(squares.get(location) instanceof BlankSquare) {
-					squares.put(location,new NumberSquare(location.intValue(),1));
-				}
-			}
-		}		
-		
-		//Each Square Knows its Neighbors for reveal
-		for(int location =0;location<numberOfSquares;location++) {
-
-			Square square = (Square)squares.get(new Integer(location));
-			
-			//Find Neighbor Squares.
-			List neighbors = new ArrayList();
-
-			// if location is not on left side, then add neighbors from left
-			if (location%(width)!=0) {
-				neighbors.add(new Integer(location-1));
-				if (location>(width - 1)) {
-					neighbors.add(new Integer(location-(width + 1)));
-				}
-				if (location<numberOfSquares- width) {
-					neighbors.add(new Integer(location + (width - 1)));
-				}
-			}
-
-			// if location is not on right side, then add neighbors from right
-			if ((location+1)%(width)!=0){ 
-				neighbors.add(new Integer(location+1));
-				if (location>width - 1) {
-					neighbors.add(new Integer(location-(width - 1)));
-				}
-
-				if (location<numberOfSquares - width) {
-					neighbors.add(new Integer(location + (width +1 ) ));
-				}
-			}
-
-			// if location is not on top, then add neighbors from top
-			if (location> width -1 ) {  
-				neighbors.add(new Integer(location-width));
-			}
-
-			// if location is not on bottom, then add neighbors from bottom
-			if (location< numberOfSquares - width) { 
-				neighbors.add(new Integer(location+width));
-			}
-
-			for(Iterator it = neighbors.iterator();it.hasNext();) {
-				square.addNeighbor((Square)(squares.get((Integer)it.next())));
-			}
+	private void defaultAllSquaresToBlank() {
+		for(int i=0;i<numberOfSquares;i++) {
+			squares.put(new Integer(i),new BlankSquare(i));
 		}
 	}
 
