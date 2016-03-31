bbc437ee50acba9dd66113aabba80fc8ccc66512
Steve Fraser <swbfraser@gmail.com>
2016-03-30 22:38:20 -0500

    Pass in height, width, and number of mines to constructor. Constants to member variables.

--- MineSweeper/src/minesweeper/domain/MineField.java
+++ MineSweeper/src/minesweeper/domain/MineField.java
@@ -3,27 +3,35 @@ import java.util.*;
 
 class MineField {
 
-	private static final int BOARD_WIDTH = 10;
-	private static final int NUMBER_OF_MINES = 10;
-	private static final int NUMBER_OF_SQUARES = 100;
+	private int width = 10;
+	private int mines = 10;
+	private int numberOfSquares = 100;
 	private Map squares;
 	private List mineLocations; 
 
-	MineField() { 
-		squares = new HashMap(NUMBER_OF_SQUARES);
+	MineField(){
+		this( 10, 10, 10);
+	}
+	
+	MineField(int width, int height, int mines ) { 
+		this.width = width;
+		this.mines = mines;
+		numberOfSquares = width * height;
+				
+		squares = new HashMap(numberOfSquares);
 
 		//Default all square to blank
-		for(int i=0;i<NUMBER_OF_SQUARES;i++) {
+		for(int i=0;i<numberOfSquares;i++) {
 			squares.put(new Integer(i),new BlankSquare(i));
 		}
 
 		//Generate Random mine locations	
-		mineLocations = new ArrayList(NUMBER_OF_MINES);
-		for(int i=0;i<NUMBER_OF_MINES;i++) {
+		mineLocations = new ArrayList(mines);
+		for(int i=0;i<mines;i++) {
 			Integer random = null;
 
 			do {
-				random = new Integer((int)(Math.random()*NUMBER_OF_SQUARES));
+				random = new Integer((int)(Math.random()*numberOfSquares));
 			}while(mineLocations.contains(random));
 
 			mineLocations.add(random);
@@ -40,36 +48,36 @@ class MineField {
 			List neighbors = new ArrayList();
 
 			// if location is not on left side, then add neighbors from left
-			if (location%BOARD_WIDTH!=0) {
+			if (location%width!=0) {
 				neighbors.add(new Integer(location-1));
-				if (location>BOARD_WIDTH-1) {
-					neighbors.add(new Integer(location-(BOARD_WIDTH + 1)));
+				if (location>width-1) {
+					neighbors.add(new Integer(location-(width + 1)));
 				}
-				if (location<NUMBER_OF_SQUARES - BOARD_WIDTH) {
-					neighbors.add(new Integer(location+(BOARD_WIDTH-1)));
+				if (location<numberOfSquares - width) {
+					neighbors.add(new Integer(location+(width-1)));
 				}
 			}
 
 			// if location is not on right side, then add neighbors from right
-			if ((location+1)%(BOARD_WIDTH)!=0){ 
+			if ((location+1)%(width)!=0){ 
 				neighbors.add(new Integer(location+1));
-				if (location>BOARD_WIDTH-1) {
-					neighbors.add(new Integer(location-(BOARD_WIDTH-1)));
+				if (location>width-1) {
+					neighbors.add(new Integer(location-(width-1)));
 				}
 
-				if (location<NUMBER_OF_SQUARES-BOARD_WIDTH) {
-					neighbors.add(new Integer(location+(BOARD_WIDTH + 1)));
+				if (location<numberOfSquares-width) {
+					neighbors.add(new Integer(location+(width + 1)));
 				}
 			}
 
 			// if location is not on top, then add neighbors from top
-			if (location>BOARD_WIDTH - 1) {  
-				neighbors.add(new Integer(location-BOARD_WIDTH));
+			if (location>width - 1) {  
+				neighbors.add(new Integer(location-width));
 			}
 
 			// if location is not on bottom, then add neighbors from bottom
-			if (location< NUMBER_OF_SQUARES - BOARD_WIDTH) { 
-				neighbors.add(new Integer(location+BOARD_WIDTH));
+			if (location< numberOfSquares - width) { 
+				neighbors.add(new Integer(location+width));
 			}
 
 				
@@ -89,7 +97,7 @@ class MineField {
 		}		
 		
 		//Each Square Knows its Neighbors for reveal
-		for(int location =0;location<NUMBER_OF_SQUARES;location++) {
+		for(int location =0;location<numberOfSquares;location++) {
 
 			Square square = (Square)squares.get(new Integer(location));
 			
@@ -97,36 +105,36 @@ class MineField {
 			List neighbors = new ArrayList();
 
 			// if location is not on left side, then add neighbors from left
-			if (location%(BOARD_WIDTH)!=0) {
+			if (location%(width)!=0) {
 				neighbors.add(new Integer(location-1));
-				if (location>(BOARD_WIDTH - 1)) {
-					neighbors.add(new Integer(location-(BOARD_WIDTH + 1)));
+				if (location>(width - 1)) {
+					neighbors.add(new Integer(location-(width + 1)));
 				}
-				if (location<NUMBER_OF_SQUARES- BOARD_WIDTH) {
-					neighbors.add(new Integer(location + (BOARD_WIDTH - 1)));
+				if (location<numberOfSquares- width) {
+					neighbors.add(new Integer(location + (width - 1)));
 				}
 			}
 
 			// if location is not on right side, then add neighbors from right
-			if ((location+1)%(BOARD_WIDTH)!=0){ 
+			if ((location+1)%(width)!=0){ 
 				neighbors.add(new Integer(location+1));
-				if (location>BOARD_WIDTH - 1) {
-					neighbors.add(new Integer(location-(BOARD_WIDTH - 1)));
+				if (location>width - 1) {
+					neighbors.add(new Integer(location-(width - 1)));
 				}
 
-				if (location<NUMBER_OF_SQUARES - BOARD_WIDTH) {
-					neighbors.add(new Integer(location + (BOARD_WIDTH +1 ) ));
+				if (location<numberOfSquares - width) {
+					neighbors.add(new Integer(location + (width +1 ) ));
 				}
 			}
 
 			// if location is not on top, then add neighbors from top
-			if (location> BOARD_WIDTH -1 ) {  
-				neighbors.add(new Integer(location-BOARD_WIDTH));
+			if (location> width -1 ) {  
+				neighbors.add(new Integer(location-width));
 			}
 
 			// if location is not on bottom, then add neighbors from bottom
-			if (location< NUMBER_OF_SQUARES - BOARD_WIDTH) { 
-				neighbors.add(new Integer(location+BOARD_WIDTH));
+			if (location< numberOfSquares - width) { 
+				neighbors.add(new Integer(location+width));
 			}
 
 			for(Iterator it = neighbors.iterator();it.hasNext();) {
@@ -146,9 +154,9 @@ class MineField {
 
 		String board = "";
 
-		for(int i=0;i< NUMBER_OF_SQUARES / BOARD_WIDTH ;i++) {
-			for(int j=0;j<BOARD_WIDTH;j++) {
-				board = board + "|"+ squares.get(new Integer(j+(i*BOARD_WIDTH)));
+		for(int i=0;i< numberOfSquares / width ;i++) {
+			for(int j=0;j<width;j++) {
+				board = board + "|"+ squares.get(new Integer(j+(i*width)));
 			}
 			board = board + "\n";
 		}
