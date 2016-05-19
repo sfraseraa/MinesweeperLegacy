5156089b0b0de749570d96a880e49b89636e4551
Steve Fraser <steve.fraser@improving.com>
2016-05-19 01:04:08 -0500

    Removed old sprout tests.

--- MineSweeper/test/minesweeper/domain/CreateMineFieldTests.java
+++ MineSweeper/test/minesweeper/domain/CreateMineFieldTests.java
@@ -35,69 +35,6 @@ public class CreateMineFieldTests {
 		}
 		assertEquals(10, mineCount);	
 	}
-
-	@Test
-	public void shouldCalculateTypesOfSquaresFromMineSquares(){
-		MineField subject = new MineField(){
-			public List generateMineLocations(){
-				return Arrays.asList(new Integer[]{
-						32,33,34,42,44,53,37,46,64,88
-				});
-			}
-		};
-		
-		Map squaresMap = subject.getSquares();
-		
-		assertTrue( squaresMap.get(32) instanceof MineSquare);
-		assertTrue( squaresMap.get(31) instanceof NumberSquare);
-		assertTrue( squaresMap.get(30) instanceof BlankSquare);
-		assertEquals( 2, ((NumberSquare)squaresMap.get(31)).getValue());
-		
-	}
-	
-	@Test
-	public void shouldHaveNoMinesWhenNoNumbersGenerated(){
-		MineField subject = new MineField(){
-			public List generateMineLocations(){
-				return Arrays.asList(new Integer[]{
-						
-				});
-			}
-		};
-		
-		Map squaresMap = subject.getSquares();
-		
-		int mineCount = 0;
-		for( Object square :  squaresMap.values() ){
-			if( square instanceof MineSquare ){
-				mineCount++;
-			}
-		}
-		assertEquals(0, mineCount);
-	}
-	
-	@Test
-	public void shouldHaveAllMinesWhen100NumbersGenerated(){
-		MineField subject = new MineField(){
-			public List generateMineLocations(){
-				Integer allSquaresMines[] = new Integer[100];
-				for( int i=0; i<100; i++){
-					allSquaresMines[i]=i;
-				}
-				return Arrays.asList(allSquaresMines);	
-			}
-		};
-		
-		Map squaresMap = subject.getSquares();
-		
-		int mineCount = 0;
-		for( Object square :  squaresMap.values() ){
-			if( square instanceof MineSquare ){
-				mineCount++;
-			}
-		}
-		assertEquals(100, mineCount);
-	}
 	
 	private class TestReplacingMinefieldConstructor extends MineField {
 		public TestReplacingMinefieldConstructor(int width, int height, int mines){
@@ -183,7 +120,7 @@ public class CreateMineFieldTests {
 	
 	
 	@Test
-	public void shouldCalculateTypesOfSquaresFromMineSquares2(){
+	public void shouldCalculateTypesOfSquaresFromMineSquares(){
 		MineField subject = new MineField(10,10, 
 				new TestNumberGenerator(new Integer[]{
 						32,33,34,42,44,53,37,46,64,88
@@ -200,7 +137,7 @@ public class CreateMineFieldTests {
 	}
 	
 	@Test
-	public void shouldHaveNoMinesWhenNoNumbersGenerated2(){
+	public void shouldHaveNoMinesWhenNoNumbersGenerated(){
 		MineField subject = new MineField(10,10, 
 			new TestNumberGenerator(new Integer[]{}));
 		
@@ -216,7 +153,7 @@ public class CreateMineFieldTests {
 	}
 	
 	@Test
-	public void shouldHaveAllMinesWhen100NumbersGenerated2(){
+	public void shouldHaveAllMinesWhen100NumbersGenerated(){
 		
 		Integer allSquaresMines[] = new Integer[100];
 		for( int i=0; i<100; i++){
